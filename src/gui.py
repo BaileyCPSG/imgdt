b3b2ac0642427c2f8e1a4d4db9ada161ab97353f
BaileyCPSG <bailey@buypowersportsgear.com>
2022-04-27 15:47:24 -0500

    Added progress bar and loggerView

--- src/gui.py
+++ src/gui.py
@@ -1,44 +1,78 @@
+import imgdt
+from tkinter import scrolledtext
 import tkinter as tk
 from tkinter import ttk
-import imgdt
 
 class App(tk.Tk):
+
     def __init__(self):
         super().__init__()
 
+        # Main Frame that all Widgets go inside
         self.mainframe = ttk.Frame(self, padding="3 3 12 12")
         self.mainframe.grid(column=0, row=0)
 
-        self.inputLabel = ttk.Label(self.mainframe, text="Input File:", width=15)
+        # Frame for all the input Widgets
+        self.inputFrame = ttk.Frame(self.mainframe)
+        self.inputFrame.grid(column=0, row=0)
+
+        # Frame for all the progress Widgets
+        self.progressFrame = ttk.Frame(self.mainframe, padding="0 10 0 0")
+        self.progressFrame.grid(column=0, row=1)
+
+        # Frame for all Loggin Widgets
+        self.loggerFrame = ttk.Frame(self.mainframe, padding = "0 10 0 0")
+        self.loggerFrame.grid(column=0, row=2)
+
+        # Frame for the Start and Exit Buttons
+        self.buttonFrame = ttk.Frame(self.inputFrame)
+        self.buttonFrame.grid(column=1, row=2)
+
+
+        # Input CSV Widgets
+        self.inputLabel = ttk.Label(self.inputFrame, text="Input File:", width=18)
         self.inputLabel.grid(column=0, row=0, sticky=tk.W)
 
-        self.inputTextbox = ttk.Entry(self.mainframe, width=50)
+        self.inputTextbox = ttk.Entry(self.inputFrame, width=50)
         self.inputTextbox.grid(column=1, row=0)
 
-        self.inputBrowseButton = ttk.Button(self.mainframe, text="Browse...", command=(lambda: imgdt.getInputFile(self.inputTextbox)))
+        self.inputBrowseButton = ttk.Button(self.inputFrame, text="Browse...", command=(lambda: imgdt.getInputFile(self.inputTextbox)))
         self.inputBrowseButton.grid(column=2, row=0)
 
 
-        self.imageDirLabel = ttk.Label(self.mainframe, text="Image Folder:")
+        # Image Directory Widgets
+        self.imageDirLabel = ttk.Label(self.inputFrame, text="Image Folder:", width=18)
         self.imageDirLabel.grid(column=0, row=1, sticky=tk.W)
 
-        self.imageDirTextbox = ttk.Entry(self.mainframe, width=50)
+        self.imageDirTextbox = ttk.Entry(self.inputFrame, width=50)
         self.imageDirTextbox.grid(column=1, row=1)
 
-        self.imageDirButton = ttk.Button(self.mainframe, text="Browse...", command=(lambda: imgdt.getImageDir(self.imageDirTextbox)))
+        self.imageDirButton = ttk.Button(self.inputFrame, text="Browse...", command=(lambda: imgdt.getImageDir(self.imageDirTextbox)))
         self.imageDirButton.grid(column=2, row=1)
 
 
-        self.resizeFlag = tk.StringVar()
-        self.resizeCheckButton = ttk.Checkbutton(self.mainframe, text="Resize", variable=self.resizeFlag, onvalue=True, offvalue=False)
+        # Resize Checkbox Widgets
+        self.resizeFlag = tk.IntVar()
+        self.resizeCheckButton = ttk.Checkbutton(self.inputFrame, text="Resize", variable=self.resizeFlag, onvalue=True, offvalue=False)
         self.resizeCheckButton.grid(column=0, row=2, sticky=tk.W)
 
 
-        self.buttonFrame = ttk.Frame(self.mainframe)
-        self.buttonFrame.grid(column=1, row=2)
+        # Start and Exit Button Widgets
+        self.startButton = ttk.Button(self.buttonFrame, text="Start", command=(lambda: imgdt.start(self)))
+        self.startButton.grid(column=0, row=0)
 
-        self.goButton = ttk.Button(self.buttonFrame, text="Go", command=(lambda: imgdt.start(self)))
-        self.goButton.grid(column=0, row=0)
+        self.exitButton = ttk.Button(self.buttonFrame, text="Exit", command=(lambda: self.quit()))
+        self.exitButton.grid(column=1, row=0)
 
-        self.cancelButton = ttk.Button(self.buttonFrame, text="Cancel", command=(lambda: self.quit()))
-        self.cancelButton.grid(column=1, row=0)
+        
+        # Progress Bar Widgets
+        self.progressLabel = ttk.Label(self.progressFrame, text="Waiting:", width=18)
+        self.progressLabel.grid(column=0, row=0, sticky=tk.W)
+
+        self.progressBar = ttk.Progressbar(self.progressFrame, orient="horizontal", mode="determinate", length=380)
+        self.progressBar.grid(column= 1, row = 0)
+
+
+        # Logger Widgets
+        self.loggerView = scrolledtext.ScrolledText(self.loggerFrame, state="disabled")
+        self.loggerView.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
--- src/imgdt.py
+++ src/imgdt.py
@@ -3,6 +3,7 @@ from tkinter import filedialog as fd
 from tkinter.messagebox import showerror, showinfo
 from PIL import Image
 import requests
+from math import floor
 import os
 import csv
 
@@ -12,10 +13,17 @@ errfile = open(errfilepath, 'w')
 sucfilepath = os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), os.path.pardir), "success.txt"))
 sucfile = open(sucfilepath, 'w')
 
-def resizeAllImages(imagedir):
+def writeLoggerView(message, app):
+    app.loggerView['state'] = "normal"
+    app.loggerView.insert(tk.END, message)
+    app.loggerView['state'] = "disabled"
+
+def resizeAllImages(imagedir, app):
     errfile.write("\nINFO: Resizing Started\n")
+    writeLoggerView("INFO: Resizing Started\n", app)
     imgs = os.listdir(imagedir)
     counter = 0
+    valuecnt = 0
     for x in range(len(imgs)):
         try:
             fp = imagedir + imgs[x]
@@ -24,19 +32,30 @@ def resizeAllImages(imagedir):
             img.save(fp)
             img.close()
             sucfile.write(f"RESIZE: {fp} was resized successfully!\n")
+            writeLoggerView(f"RESIZE: {fp} was resized successfully!\n", app)
             counter += 1
         except Exception as e:
             errfile.write(f"RESIZE: {e}\n")
+            writeLoggerView(f"RESIZE: {e}\n", app)
+        valuecnt += 1
+        value = floor((valuecnt / len(imgs) * 100))
+        app.progressBar['value'] = value
+        app.progressLabel['text'] = f"Resizing {value}%"
+        app.update()
+    sucfile.write(f"\nRESIZE: {counter}/{len(imgs)} files resized successfully!\n\n")
+    writeLoggerView(f"RESIZE: {counter}/{len(imgs)} files resized successfully!\n", app)
     showinfo(title="Success", message=(f"\n{counter}/{len(imgs)} files were resized!\nCheck the log file for errors!"))
 
-def hasFileExt(filenames):
+def hasFileExt(filenames, app):
     flag = True
     errfile.write("INFO: File Extension Check Started\n")
+    writeLoggerView("INFO: File Extension Check Started\n", app)
     fileExts = ["jpg", "png", "peg"]
     for f in filenames:
         ext = f[-3:]
         if not ext in fileExts:
             errfile.write(f"FILEEXT: {f} has the wrong file extension!\n")
+            writeLoggerView(f"FILEEXT: {f} has the wrong file extension!\n", app)
             flag = False
     return flag
 
@@ -66,20 +85,24 @@ def start(app):
         # Check for blank input fields
         if csvfilepath == "" or imagedir == "":
             errfile.write("ERROR: Locations cannot be null\n")
+            writeLoggerView("ERROR: Locations cannot be null\n", app)
             showerror(title="File Error", message="File locations cannot be blank!")
             return
 
         # Check if exists
-        if os.path.exists(csvfilepath) and os.path.isfile(csvfilepath) and os.path.exists(imagedir) and os.path.isdir(imagedir):
+        if os.path.exists(csvfilepath) and os.path.isfile(csvfilepath) and os.path.exists(imagedir) and os.path.isdir(imagedir) and (imagedir[-1] == "\\" or imagedir[-1] == "/"):
             errfile.write("INFO: Starting Script...\n")
+            writeLoggerView("INFO: Starting Script...\n", app)
         else:
             showerror(title="Error", message="Cannot open file or directory. Please double check spelling.")
-            errfile.write("Error: Cannot open file or directory. Please double check spelling.\n")
+            errfile.write("ERROR: Cannot open file or directory. Please double check spelling.\n")
+            writeLoggerView("Error: Cannot open file or directory. Please double check spelling.\n", app)
             return
 
     except Exception as e:
         showerror(title="Error", message="Cannot open file or directory. Please double check spelling.")
         errfile.write("Error: Cannot open file or directory. Please double check spelling.\n")
+        writeLoggerView("Error: Cannot open file or directory. Please double check spelling.\n", app)
         stop(-1)
 
 
@@ -92,11 +115,13 @@ def start(app):
             # if headers are bad then try opening with utf-8 encoding and check headers again
             csvfile.close()
             sucfile.write("INFO: Opening file with UTF-8 encoding.\n")
+            writeLoggerView("INFO: Opening file with UTF-8 encoding.\n", app)
             csvfile = open(csvfilepath, "r", encoding="utf-8-sig")
             reader = csv.DictReader(csvfile)
         if reader.fieldnames != ['url', 'filename']:
             #exit for bad headers
             errfile.write("ERROR: File must have the headers: url,filename\n")
+            writeLoggerView("ERROR: File must have the headers: url,filename\n", app)
             showerror(title="Error", message="File must have the headers: url,filename")
             stop(-1)
         for row in reader:
@@ -104,13 +129,15 @@ def start(app):
             urls.append(row['url'])
 
         # after lists are loaded, check for filename extensions
-        if not hasFileExt(fns):
+        if not hasFileExt(fns, app):
             showerror(title="Error", message="Filenames must include extensions\nPlease check the log file for more info!\n")
             stop(-1)
 
         errfile.write("\nINFO: Downloading Started\n")
+        writeLoggerView("INFO: Downloading Started\n", app)
 
         counter = 0
+        valuecnt = 0
         for x in range(len(urls)):
             try:
                 res = requests.get(urls[x], stream=True)
@@ -119,19 +146,30 @@ def start(app):
                         for chunk in res:
                             outFile.write(chunk)
                     sucfile.write(f"DOWNLOAD: {fns[x]} downloaded successfully!\n")
+                    writeLoggerView(f"DOWNLOAD: {fns[x]} downloaded successfully!\n", app)
                     counter += 1
                 else:
                     errfile.write(f"DOWNLOAD: {urls[x]} had status code: {res.status_code}\n")
+                    writeLoggerView(f"DOWNLOAD: {urls[x]} had status code: {res.status_code}\n", app)
             except Exception as e:
                 print(e)
                 errfile.write(f"DOWNLOAD: {e}\n")
-        sucfile.write(f"\nDOWNLOAD:{counter}/{len(urls)} files downloaded successfully!\n")
+                writeLoggerView(f"DOWNLOAD: {e}\n", app)
+            valuecnt += 1
+            value = floor((valuecnt / len(urls)) * 100)
+            app.progressBar['value'] = value
+            app.progressLabel['text'] = f"Downloading {value}%"
+            app.update()
+        sucfile.write(f"\nDOWNLOAD:{counter}/{len(urls)} files downloaded successfully!\n\n")
+        writeLoggerView(f"DOWNLOAD:{counter}/{len(urls)} files downloaded successfully!\n", app)
         showinfo(title="Success", message=(f"{counter}/{len(urls)} files downloaded successfully!\nCheck the log file for errors."))
-        
-        if app.resizeFlag.get():
-            resizeAllImages(imagedir)
 
-        stop(0)
+        if app.resizeFlag.get():
+            resizeAllImages(imagedir, app)
+
+        app.progressLabel['text'] = "Done"
+        return
 
     except Exception as e:
         errfile.write(f"{e}\n")
+        writeLoggerView(f"{e}\n", app)
