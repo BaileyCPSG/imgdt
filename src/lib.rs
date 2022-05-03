7198a9f5e3f6564436eb25ca337d85939b4343ab
Bailey <bfeld.111@gmail.com>
2022-05-02 23:13:38 -0500

    Tried to add reqwest

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use clap::Parser;
+use reqwest::Client;
+use serde::Deserialize;
+use std::error::Error;
+
+/// Download images from a csv file with Rust!
+#[derive(Debug, Parser)]
+#[clap(author, version)]
+pub struct Config {
+    /// path to the input file
+    pub input_file: String,
+
+    /// directory to store the downloaded files
+    #[clap(short='o', default_value=".")]
+    pub download_dir: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Record {
+    url: String,
+    filename: String,
+}
+
+pub async fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+    let mut rdr = csv::Reader::from_path(&config.input_file)?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+        client.get(record.url).send().await?;
+    }
+
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn add_test() {
+        assert_eq!(4, 2+2);
+    }
+}
